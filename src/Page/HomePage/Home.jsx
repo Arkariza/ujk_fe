60bff1aefefe9b0b598a87e13915564f5ce402a7
Arkariza <zex3375@gmail.com>
2026-06-09 09:08:16 +0700

    menghapus dan merapihkan code

--- src/Page/HomePage/Home.jsx
+++ src/Page/HomePage/Home.jsx
@@ -21,7 +21,6 @@ export default function HomePage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const token = localStorage.getItem("token")
         const res = await axios.get(`${API_CRUD}/get`, {
         })
         setItems(res.data?.data ?? [])
